6b46b825b906e41ad44b60a6218ea6fdca3f25b7
苏日甫 <1020343499@qq.com>
2016-06-20 11:05:36 +0800

    显示输出a+b
    在Swift中，不需要严格定义变量的类型。直接用var定义就可以。如果需要定义类型可以这样写：var a：int =
    2这样就严格定义成int类型了。let定义为常量，它在运行过程中值是不会变的。 直接用printf输出。

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  1
+//
+//  Created by 20141105039 on 16/6/20.
+//  Copyright © 2016年 20141105039. All rights reserved.
+//
+
+import Foundation
+var a = 2
+var b = 3
+let c = a+b
+print(c)
+
